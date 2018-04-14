07f5d30dd551a8b599168c219994f4d0a86a4924
Samantha Rabling <sjarillorabling@gmail.com>
2018-04-13 21:23:53 -0700

    starting almost done with controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,24 @@
 class UsersController < ApplicationController
   def new
   end
-  
+
   def create
   @user = User.new(params.require(:user).permit(:first_name, :last_name, :email))
   if @user.save
     flash[:notice] = ['User successfully created']
     redirect_to new_user_path
   else
-    #errors we need to code later
+    if not params[:first_name]
+      flash[:notice1]="First name can't be blank"
+    end
+
+    if not params[:last_name]
+      flash[:notice2]="Last name can't be blank"
+    end
+
+    if not params[:email]
+      flash[:notice3]="Email can't be blank"
+    end
   end
 end
 end
